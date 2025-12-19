a10141cfce86107052a18f522156b0f3aef2524e
BlueMoon0604 <navyasaxena32@gmail.com>
2025-12-19 11:31:57 +0530

    Refactor some problems

--- core-java-practice/gcr-codebase/programming-elements/Celsius.java
+++ core-java-practice/gcr-codebase/programming-elements/Celsius.java
@@ -4,8 +4,8 @@ public class Celsius {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int cel = sc.nextInt();
-        int Fahrenheit = (cel * 9/5) + 32 ;
-        System.out.println("Temperature in Fahrenheit is : " + Fahrenheit);
+        int fahrenheit = (cel * 9/5) + 32 ;
+        System.out.println("Temperature in Fahrenheit is : " + fahrenheit);
     }
     
 }
