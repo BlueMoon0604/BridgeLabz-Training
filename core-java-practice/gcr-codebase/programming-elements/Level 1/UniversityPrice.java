6cba35e9775fb2a701a472111cc9b94b07f9bc8d
Navya Saxena <navyasaxena32@gmail.com>
2025-12-19 11:07:30 +0530

    Update UniversityPrice.java

--- core-java-practice/gcr-codebase/programming-elements/Level 1/UniversityPrice.java
+++ core-java-practice/gcr-codebase/programming-elements/Level 1/UniversityPrice.java
@@ -2,9 +2,9 @@ public class UniversityPrice {
     public static void main(String[] args) {
         int price = 125000;
         int percent = 10;
-        int DiscountAmount = (125000 * percent ) / 100;
-        int DiscountPrice = price - DiscountAmount;
-        System.out.println("The discount Amount is INR " + DiscountAmount + " and The final Discounted fee is INR " + DiscountPrice);
+        int discountAmount = (125000 * percent ) / 100;
+        int discountPrice = price - discountAmount;
+        System.out.println("The discount Amount is INR " + discountAmount + " and The final Discounted fee is INR " + discountPrice);
     }
     
 }
