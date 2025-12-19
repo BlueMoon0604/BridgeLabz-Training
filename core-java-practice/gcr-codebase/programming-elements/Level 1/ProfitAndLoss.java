53b27e5628999a12ba22ca1509c27d68320cc7ea
BlueMoon0604 <navyasaxena32@gmail.com>
2025-12-19 11:16:10 +0530

    [Navya]modified files

--- core-java-practice/gcr-codebase/programming-elements/Level 1/ProfitAndLoss.java
+++ core-java-practice/gcr-codebase/programming-elements/Level 1/ProfitAndLoss.java
@@ -1,12 +1,12 @@
 public class ProfitAndLoss {
     public static void main(String[] args) {
-        double CostPrice = 129;
-        double SellingPrice = 191;
-        double Profit  = SellingPrice - CostPrice;
-        double Loss= CostPrice - SellingPrice;
-        double ProPercent = (Profit / CostPrice) * 100 ;
-        double LossPercent = (Loss / CostPrice) * 100 ;
-        System.out.println("The Cost Price is INR " + CostPrice + " and Selling Price is INR " + SellingPrice +"\n" + "The profit is INR " + Profit + "and The Profit Percent is " + ProPercent + "\n" + "The Loss is INR " + Loss + "and The Loss Percent is " + LossPercent );
+        double costPrice = 129;
+        double sellingPrice = 191;
+        double profit  = sellingPrice - costPrice;
+        double loss= costPrice - sellingPrice;
+        double proPercent = (profit / costPrice) * 100 ;
+        double lossPercent = (loss / costPrice) * 100 ;
+        System.out.println("The Cost Price is INR " + costPrice + " and Selling Price is INR " + sellingPrice +"\n" + "The profit is INR " + profit + "and The Profit Percent is " + proPercent + "\n" + "The Loss is INR " + loss + "and The Loss Percent is " + lossPercent );
 
     }
      
