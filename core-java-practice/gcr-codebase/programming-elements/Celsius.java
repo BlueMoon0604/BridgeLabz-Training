e399ce9deb1b6d0e943e25f2b13fe92cd70305df
Navya Saxena <navyasaxena32@gmail.com>
2025-12-19 11:03:29 +0530

    Update Celsius.java

--- core-java-practice/gcr-codebase/programming-elements/Celsius.java
+++ core-java-practice/gcr-codebase/programming-elements/Celsius.java
@@ -4,7 +4,7 @@ public class Celsius {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int cel = sc.nextInt();
-        int Fahrenheit = (cel * 9/5) + 32 ;
+        int fahrenheit = (cel * 9/5) + 32 ;
         System.out.println("Temperature in Fahrenheit is : " + Fahrenheit);
     }
     
