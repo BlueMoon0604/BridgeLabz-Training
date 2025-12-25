bb60ff59fa5f09f33a76290c1bd850c66fee3d9f
BlueMoon0604 <navyasaxena32@gmail.com>
2025-12-25 15:42:30 +0530

    [Navya] adding level 2 from methods

---
+++ core-java-practice/gcr-codebase/methods/LeapYear.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LeapYear {
+	
+	public static void computeLeapYear(int year) {
+		if(year >= 1582) {
+			if(year % 4 == 0) {
+				System.out.println(" It's a Leap year " );
+			}else if(year % 100 != 0) {
+				System.out.println("It's not a leap year");
+			}else if(year % 400 ==0) {
+				System.out.println(" It's a Leap year " );
+			}
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		computeLeapYear(year);
+		
+		
+	}
+}
